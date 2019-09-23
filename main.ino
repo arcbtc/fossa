88fd95ae5ecb48359832e380ef0a744d3aa011cb
Arc <33088785+arcbtc@users.noreply.github.com>
2019-09-23 18:06:01 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,122 +1,180 @@
 /**
- *  The Fossa uses Zap desktop wallet tunnelled through serveo.net "ssh -R SOME-RANDOM-WORD.serveo.net:3010:localhost:8180 serveo.net"
- *  
+ *  Physically Faucet uses Zap desktop wallet tunnelled through serveo.net "ssh -R SOME-NAME.serveo.net:3010:localhost:8180 serveo.net"
+ *  Due to using an admin macaroon, an LND instance with limited funds is preferable, Zap desktop wallet is ideal
  *  Wiring - ESP32 NodeMCU 32s + ST7735 TFT 1.8
  *
- *  Epaper PIN MAP: [VCC - 3.3V, GND - GND, CS - GPIO5, Reset - GPIO16, AO (DC) - GPI17, SDA (MOSI) - GPIO23, SCK - GPIO18, LED - GPIO4]
- *       
- *
+ *  3.5inch TFT PIN MAP: [VCC - 3.3V, GND - GND, CS - GPIO5, Reset - GPIO16, AO (DC) - GPI17, SDA (MOSI) - GPIO23, SCK - GPIO18, LED - GPIO4]
  */
 
-#include <WiFiClientSecure.h>
-#include <ArduinoJson.h> 
-#include "qrcode.h"
-#include <SPI.h>
-#include "Ucglib.h"
+#include "xbm.h"             // Sketch tab header for xbm images
 
-Ucglib_ST7735_18x128x160_HWSPI ucg(/*cd=*/ 17, /*cs=*/ 5, /*reset=*/ 16);
+#include <TFT_eSPI.h>        // Hardware-specific library
+
+#include <WiFiClientSecure.h>
+
+#include <ArduinoJson.h>
+
+#include "qrcode.h"
+
+#include <SPI.h>
+
+#include <math.h>
+
+
+TFT_eSPI tft = TFT_eSPI();   // Invoke library
 
 String giftinvoice;
 String giftid;
 String giftlnurl;
-String amount;
-
 bool spent = false;
 String giftstatus = "unpaid";
 unsigned long tickertime;
 unsigned long tickertimeupdate;
 unsigned long giftbreak = 600000;  
 unsigned long currenttime = 600000;
+String amount;
+int satoshis;
+String timeleft;
+int countdown;
+int fee = 2; //percentage fee your going to take
+float feefloat = fee/100;
 
-//enter your wifi details
-char wifiSSID[] = "YOUR-WIFI";
-char wifiPASS[] = "YOUR-WIFI-PASSWORD";
-
+float conversion;
+String on_currency = "BTCEUR";
+int httpsPort = 443;
 const char* gifthost = "api.lightning.gifts";
-const int httpsPort = 443;
 
-const char* lndhost = "SOME-RANDOM-WORD.serveo.net"; //if using serveo.net replace the *SOME-RANDOM-WORD* bit
-String adminmacaroon = "YOUR-LND-ADMIN-MACAROON"; //obv, this is dodge, so use an instance of LND with limited funds, I use desktop Zap wallet and make it available through serveo.net 
+const char* lndhost = "arcbtc.serveo.net"; //if using serveo.net replace the *SOME-RANDOM-WORD* bit
+String adminmacaroon = "YOUR-ADMIN-MACAROON"; //obv, this is dodge, so use an instance of LND with limited funds, I use desktop Zap wallet and make it available through serveo.net (https://docs.zaphq.io/docs-desktop-neutrino-connect is a useful doc) 
 const int lndport = 3010;
 
-String on_currency = "BTCEUR";  //currency can be changed here ie BTCEUR BTCGBP etc
-String on_sub_currency = on_currency.substring(3);
+String accepts = "10 20 50 1 2";
 
+//Wifi details
+char wifiSSID[] = "YOUR-WIFI";
+char wifiPASS[] = "YOUR-WIFI-PASS";
 
 // Constants
-const int coinpin = 15;
-int totes;
+const int coinpin = 4;
+float totes;
 int counta;
 unsigned long prevmils = 0;
 unsigned long tempmils = 0;
-float conversion;
-
-bool checker = false;
 
 
-void setup() {
- 
-  delay(1000);
-  ucg.begin(UCG_FONT_MODE_TRANSPARENT);
-  ucg.setFont(ucg_font_ncenR14_hr);
-  ucg.clearScreen();
-  
+void setup()
+{
+  pinMode(coinpin, INPUT_PULLUP);
   Serial.begin(115200);
-         
+  
+  tft.begin();               // Initialise the display
+  tft.fillScreen(TFT_BLACK); // Black screen fill
+  tft.setRotation(3);
+ 
+  tft.setTextColor(TFT_WHITE);  // Set text colour to black, no background (so transparent)
+  tft.setTextSize(2); 
+  //connect to local wifi            
   WiFi.begin(wifiSSID, wifiPASS);   
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("connecting");
+
     delay(2000);
   }
-
-  nodecheck();
-  ucg.setFont(ucg_font_helvB10_hr);
-  ucg.setPrintPos(0,20);
 
   attachInterrupt(digitalPinToInterrupt(coinpin), coinInterrupt, RISING);
 
   on_rates();
-  
+
 }
 
+
 void loop() {
+  nodecheck();
+  splash();
+
   if (millis() - prevmils > 3000 && counta == 1){
+     tft.fillScreen(TFT_BLACK);
    Serial.println(totes);
    
    counta = 2;
   }
   if (counta == 2){
-  float satoshis = (totes/100)/conversion;
-   int intsats = (int) round(satoshis*100000000.0);
-   amount = intsats;
-   Serial.println(intsats);
+   
+   
+   satoshis = ((totes*100000000)/conversion) * (1-feefloat);
+    
+   currentamount();
+
+   Serial.println(satoshis);
+   amount = satoshis;
    create_gift();
    Serial.println(giftinvoice); 
    makepayment();
+   prevmils = millis();
    checkgift();
+   
    while(giftstatus == "unpaid"){
+    
     checkgiftstatus();
-    delay(500);
+    delay(2000);
+    
   }
    showAddress(giftlnurl);
-   while(!spent){
+   Serial.println(giftlnurl);
+   
+   tft.setTextColor(TFT_WHITE);
+   tft.setCursor(2, 220, 1);
+   tft.print("invoice expires in:120secs");
+   
+   while(spent == false){
+    
+    
+    tft.setTextColor(TFT_WHITE);
+    tft.setCursor(230, 220, 1);
+    tft.print(timeleft + "secs");
+    
+    tft.setTextColor(TFT_BLACK);
+    tft.setCursor(2, 220, 1);
+    countdown = int(120 - ((millis() - prevmils)/1000));
+    
+    timeleft = countdown;
+    tft.print("invoice expires in:");
+    tft.setCursor(230, 220, 1);
+    tft.print(timeleft + "secs");
+    
     checkgift();
-    delay(500);
+    delay(2000);
+    if (countdown <= 3){
+      spent = true;
+      Serial.println(spent);
+    }
   }
    
    counta = 0;
    spent = false;
+   totes = 0;
   }
 }
 
 // Interrupt
 void coinInterrupt(){
   prevmils = millis();
-  totes = totes + 10;
+  totes = totes + 0.1;
   counta = 1;
-
+  
 }
- 
+
+void currentamount(){
+  tft.drawXBitmap(0, 0, paid, 320, 240, TFT_WHITE, TFT_BLACK);
+    tft.setTextColor(TFT_RED);
+    tft.setCursor(110, 53, 2);
+    tft.print(String(totes));
+    
+    tft.setTextColor(TFT_GREEN);
+    tft.setCursor(110, 105, 2);
+    tft.print(String(satoshis));
+    tft.setCursor(180, 117, 1);
+    tft.print(" (-" + String(fee) + "% fee)");
+}
 
 void create_gift(){
 
@@ -158,31 +216,24 @@ void create_gift(){
   giftlnurl = lnurl;
 }
 
-
 void nodecheck(){
+  bool checker = false;
+  while(!checker){
   WiFiClientSecure client;
+  
   if (!client.connect(lndhost, lndport)){
-    return;
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_RED);
+    tft.setCursor(60, 60, 2);
+    tft.println("no node detected"); 
+    delay(1000);
+  }
+  else{
+    checker = true;
+  }
   }
   
-  client.print(String("GET ")+ "https://" + lndhost +":"+ String(lndport) +"/v1/getinfo HTTP/1.1\r\n" +
-               "Host: "  + lndhost +":"+ String(lndport) +"\r\n" +
-               "User-Agent: ESP32\r\n" +
-               "Content-Type: application/json\r\n" +
-               "Connection: close\r\n\r\n");
-                 
-  String line = client.readStringUntil('\n');
-    
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      break;
-    }
-  } 
-  String content = client.readStringUntil('\n');
-  client.stop();
 }
-
 
 void makepayment(){
   String memo = "Memo-";
@@ -293,6 +344,7 @@ void on_rates(){
 }
 
 void showAddress(String XXX){
+  tft.fillScreen(TFT_WHITE); // Black screen fill
   String addr = XXX;
   int qrSize = 10;
   int sizes[17] = { 14, 26, 42, 62, 84, 106, 122, 152, 180, 213, 251, 287, 331, 362, 412, 480, 504 };
@@ -306,17 +358,66 @@ void showAddress(String XXX){
   QRCode qrcode;
   uint8_t qrcodeData[qrcode_getBufferSize(qrSize)];
   qrcode_initText(&qrcode, qrcodeData, qrSize, 1, addr.c_str());
-  ucg.setPrintPos(10,10);
-  float scale = 2;
-  int padding = 20;
-  ucg.setColor(255, 255, 255);
-  ucg.drawBox(0, 0, 200, 200);
-  ucg.setColor(0, 0, 0);
+//  ucg.setPrintPos(10,10);
+  float scale = 4;
+   
   for (uint8_t y = 0; y < qrcode.size; y++) {
     for (uint8_t x = 0; x < qrcode.size; x++) {
-      if(qrcode_getModule(&qrcode, x, y)){       
-        ucg.drawBox(padding+scale*x, 10 + padding+scale*y, scale, scale);
+      if(qrcode_getModule(&qrcode, x, y)){   
+        tft.fillRect(70+scale*x, 2 + 17+scale*y, scale, scale, TFT_BLACK);    
       }
     }
+  }
+ tft.setTextColor(TFT_BLACK);
+ tft.setCursor(2, 220, 1);
+ tft.print("invoice expires in:120secs");
+}
+
+void splash(){
+  
+  if(counta == 1){
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_GREEN);
+    tft.setCursor(80, 60, 2);
+    tft.println("PROCESSING"); 
+  }
+   else{
+  tft.drawXBitmap(0, 0, sats, 320, 90, TFT_WHITE, TFT_BLACK);
+  for (int i=0;i<=2;i++){
+   
+  tft.drawXBitmap(0, 90, bitcoin, 320, 150, TFT_YELLOW, TFT_BLACK);
+  tft.drawXBitmap(0, 90, bitcoin, 320, 150, TFT_ORANGE, TFT_BLACK);
+  }
+  }
+  if(counta == 1){
+     tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_GREEN);
+    tft.setCursor(80, 60, 2);
+    tft.println("PROCESSING");
+  }
+   else{
+    for (int i=0;i<=2;i++){
+   
+  tft.drawXBitmap(0, 90, lightning, 320, 150, TFT_YELLOW, TFT_BLACK);
+  tft.drawXBitmap(0, 90, lightning, 320, 150, TFT_ORANGE, TFT_BLACK);
+    }
+  }
+  if(counta == 1){
+     tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_GREEN);
+    tft.setCursor(80, 60, 2);
+    tft.println("PROCESSING");
+  }
+  else{
+  tft.drawXBitmap(0, 0, insert, 320, 240, TFT_WHITE, TFT_BLACK);
+  tft.setTextColor(TFT_GREEN);
+  tft.setCursor(130, 90, 1);    
+  tft.println(accepts);
+  tft.setCursor(10, 180, 1);
+  tft.println("1 BTC (-"+ String(fee) + "%) = " + String(conversion * (1-feefloat)) + " " + on_currency.substring(3));
+  tft.setCursor(10, 200, 1);
+  tft.println("1000 SAT (-" + String(fee) + "%) = " + String((conversion / 100000) * (1-feefloat)) + " " + on_currency.substring(3));
+  delay(2000);
+  tft.setTextColor(TFT_WHITE);
   }
 }
